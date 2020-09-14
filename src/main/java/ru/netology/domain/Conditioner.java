811e1185ad50c7f9ba36f75999c13a81e5c53012
Oleg Sedov <olegsedov1994@yandex.ru>
2020-09-14 21:10:50 +0300

    Fixed code

--- src/main/java/ru/netology/domain/Conditioner.java
+++ src/main/java/ru/netology/domain/Conditioner.java
@@ -7,45 +7,53 @@ public class Conditioner {
     private int currentTemperature;
     private boolean on;
 
-    // Создаем метод для увеличения температуры
     public void increaseCurrentTemperature() {
-        // Если текущая температура достигнет значения максимальной температуры, заканчиваем выполнение функции
-        if (currentTemperature == maxTemperature) { return; }
-        // Каждый раз после прохождения функции, увеличиваем текущую температуру на 1 (++)
+        if (currentTemperature == maxTemperature) {
+            return;
+        }
         this.currentTemperature++;
     }
 
-    // Создаем метод для уменьшения температуры
     public void decreaseCurrentTemperature() {
-        // Если текущая температура достигнет значения минимальной температуры, заканчиваем выполнение функции
-        if (currentTemperature == minTemperature) { return; }
-        // Каждый раз после прохождения функции, уменьшаем текущую температуру на 1 (++)
+        if (currentTemperature == minTemperature) {
+            return;
+        }
         this.currentTemperature--;
     }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getName() {
+        return name;
+    }
 
-    public int getMaxTemperature() { return maxTemperature; }
-    public void setMaxTemperature(int maxTemperature) { this.maxTemperature = maxTemperature; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public int getMinTemperature() { return minTemperature; }
-    public void setMinTemperature(int minTemperature) { this.minTemperature = minTemperature; }
+    public int getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    public void setMaxTemperature(int maxTemperature) {
+        this.maxTemperature = maxTemperature;
+    }
+
+    public int getMinTemperature() {
+        return minTemperature;
+    }
+
+    public void setMinTemperature(int minTemperature) {
+        this.minTemperature = minTemperature;
+    }
 
     public int getCurrentTemperature() {
         return currentTemperature;
     }
+
     public void setCurrentTemperature(int currentTemperature) {
-        // Если текукщая температура становится больше максимальной, то
-        // текущая температура принимает значение максимальной температуры, и
-        // Прекращается выполнение функции
         if (currentTemperature > maxTemperature) {
             this.currentTemperature = maxTemperature;
             return;
         }
-        // Если текукщая температура становится меньше минимальной, то
-        // текущая температура принимает значение минимальной температуры, и
-        // Прекращается выполнение функции
         if (currentTemperature < minTemperature) {
             this.currentTemperature = minTemperature;
             return;
@@ -53,6 +61,11 @@ public class Conditioner {
         this.currentTemperature = currentTemperature;
     }
 
-    public boolean isOn() { return on; }
-    public void setOn(boolean on) { this.on = on; }
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
 }
